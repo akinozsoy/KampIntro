f4c41284e61500c67e204f58f7e7a133300671dd
Ali Akın Özsoy <20740022@stu.omu.edu.tr>
2021-08-04 14:33:39 +0300

    10 August 2021 end off class

---
+++ Metotlar/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Add();
+            //Add();
+            //Add();
+            //Add();
+            //int result = Add2();
+
+            //int number1 = 20;
+            //int number2 = 100;
+            //var result2 = Add3(ref number1,number2);
+            //Console.WriteLine(result2);
+            //Console.WriteLine(number1);
+
+            Console.WriteLine(Multiply(2, 4));
+            Console.WriteLine(Multiply(2, 4, 5));
+            Console.WriteLine(Add4(2,3,4,5,6,7,8,9));
+            Console.ReadLine();
+        }
+
+        static void Add()
+        {
+            Console.WriteLine("Added!!");
+        }
+        static int Add2(int number1 = 30, int number2 = 30)
+        {
+            var result = number1 + number2;
+            return result;
+
+        }
+        static int Add3(ref int number1, int number2) {
+            number1 = 30;
+            return number1 + number2;
+
+
+        }
+
+        static int Multiply(int number1, int number2)
+        {
+            return number1 * number2;
+        }
+        static int Multiply(int number1, int number2, int number3)
+        {
+            return number1 * number2 * number3;
+        }
+        static int Add4(params int[] numbers)
+        {
+            return numbers.Sum();
+        }
+       
+    }
+}
