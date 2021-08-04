c4bfbbe87d3231415ddd6c025e210dbe5f4cc194
Ali Akın Özsoy <20740022@stu.omu.edu.tr>
2021-08-04 20:55:14 +0300

    3.DAY HOMEWORK

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        List<Customer> customers = new List<Customer> { };
+        public void Add(Customer customer)
+        {
+            customers.Add(customer);
+            Console.WriteLine("Müşteri Başarıyla eklendi " + customer.FirstName + " " + customer.LastName );
+        }
+        public void Remove(Customer customer)
+        {
+            customers.Add(customer);
+            Console.WriteLine("Müşteri başarıyla silindi "+customer.FirstName + " " + customer.LastName);
+        }
+
+        public void List()
+        {
+            foreach (var customer in customers)
+            {
+                Console.WriteLine("ID : "+customer.Id);
+                Console.WriteLine("First Name : " + customer.FirstName);
+                Console.WriteLine("Last Name : " + customer.LastName);
+            }
+        }
+        
+    }
+
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Id = 1;
+            customer1.FirstName = "Akın";
+            customer1.LastName = "Özsoy";
+
+            Customer customer2 = new Customer();
+            customer2.Id = 2;
+            customer2.FirstName = "Engin";
+            customer2.LastName = "Demiroğ";
+
+            Customer customer3 = new Customer();
+            customer3.Id = 3;
+            customer3.FirstName = "Kobe";
+            customer3.LastName = "Bryant";
+
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            customerManager.Add(customer2);
+            customerManager.Add(customer3);
+            Console.WriteLine("Müşteriler Eklendi");
+            Console.WriteLine(" ");
+
+            customerManager.List();
+            Console.WriteLine("Müşteriler Listelendi");
+            Console.WriteLine(" ");
+            
+            customerManager.Remove(customer1);
+            customerManager.Remove(customer2);
+            customerManager.Remove(customer3);
+        }
+    }
+}
